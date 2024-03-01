b08346cbd2f75ccd1f02c6a5d3699dbe9ee35276
4orniy Vlad <4ornii.vlad@gmail.com>
2024-03-01 10:07:00 +0200

    add message when nothing hovered

--- src/common/components/notificationsList/index.tsx
+++ src/common/components/notificationsList/index.tsx
@@ -1,28 +1,30 @@
-import type { FC} from 'react'
+import type { FC } from 'react'
 import { useRef } from 'react'
 import s from './notificationsList.module.css'
 import { useAutoScroll } from '@/common/hooks/useAutoScroll'
 
 interface INotificationsProps {
-  notifications: string[];
+  notifications: string[]
 }
 
-export const NotificationsList: FC<INotificationsProps> = ({
-  notifications,
-}) => {
+export const NotificationsList: FC<INotificationsProps> = ({ notifications }) => {
   const listRef = useRef<HTMLUListElement>(null)
   useAutoScroll(listRef.current ?? listRef.current, notifications.length)
 
   return (
     <div className={s.notificationsContainer}>
-      <h2 className={s.title}>Hover squares</h2>
-      <ul ref={listRef} className={s.list}>
-        {notifications.map((item) => (
-          <li className={s.list__item} key={item}>
-            {item}
-          </li>
-        ))}
-      </ul>
+      <h2 className={s.title}>Hovered squares</h2>
+      {notifications.length ? (
+        <ul ref={listRef} className={s.list}>
+          {notifications.map((item) => (
+            <li className={s.list__item} key={item}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>nothing was hovered yet</p>
+      )}
     </div>
   )
 }
